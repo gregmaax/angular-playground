089dd5aa4840a2268dfce74ed8be08c6f0bf4aa6
gregmaax <greg.d.972@gmail.com>
2025-03-11 17:14:25 +0100

    adding tailwindCss + taigaUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import {TuiButton, TuiRoot} from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot, TuiButton],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <tui-root>
+      <h1>Welcome to {{title}}!</h1>
+      <h1 class="text-3xl text-blue-700 font-bold underline">
+        Hello world!
+      </h1>
 
-    <router-outlet />
+      <button
+        size="s"
+        tuiButton
+        type="button"
+      >
+        Tester
+      </button>
+
+      <router-outlet />
+    </tui-root>
   `,
   styles: [],
 })
